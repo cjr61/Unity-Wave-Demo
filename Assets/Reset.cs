0584e7ff67e3f191c869256f02f79e28845d294b
cjr61 <cjr61@zips.uakron.edu>
2019-03-12 16:57:30 -0400

    Added reset button

---
+++ Assets/Reset.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+
+    public GameObject diver;
+    public GameObject water;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            var diverPos = diver.transform.position;
+            diverPos.x = 0f;
+            diverPos.y = .5f;
+            diverPos.z = -3f;
+            diver.transform.position = diverPos;
+
+            Physics.IgnoreCollision(diver.GetComponent<Collider>(), water.GetComponent<Collider>(), false);
+
+        }
+    }
+
+}
